737ac8e153f59517f4c71136a1266dd568fab9cd
khushi <khushipurohit171@gmail.com>
2026-04-10 19:01:57 +0530

    backtracking ques

---
+++ src/SORTING/bubblesort.java
@@ -0,0 +1,7 @@
+package SORTING;
+
+public class bubblesort {
+    // main idea behind bubble sort is that it checks each adjacent elem and pushes the larger element towards the end
+    // at each pass the largest elem is at its right position = sorted
+
+}
